101d73ee38ceaca6362aa98c3c59e109dd3ba5cd
Ashley Newsome <ashleynewsome37@yahoo.com>
2019-04-30 18:41:07 -0400

    WOEID part with CMD Interface

--- webscrape.py
+++ webscrape.py
@@ -3,15 +3,22 @@
 
 # Imports
 import yweather
+import argparse
 
-# Creating an object
-client = yweather.Client()
+# CMD Interface using argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-W', help='Gets the WOEID for a location', action='store_true') # Get WOEID
+args = parser.parse_args()
 
-# User Input
-user_city = str(input('Enter city: '))
-user_state = str(input('Enter State:'))
-user_location = user_city +','+ user_state
-##print(user_location)
+if args.W:
+        # Creating an object
+        client = yweather.Client()
 
-# Get WOEID
-print(client.fetch_woeid(user_location))
+        # User Input
+        user_city = str(input('Enter city: '))
+        user_state = str(input('Enter State:'))
+        user_location = user_city +','+ user_state
+        ##print(user_location)
+
+        # Get WOEID
+        print(client.fetch_woeid(user_location)) # Prints WOEID number

a9068895b0fb5f52ab3fda55312812a552d10094
AshleyN224 <ashleynewsome37@yahoo.com>
2020-06-09 23:59:14 -0400

    removed keys

--- auth.py
+++ auth.py
@@ -1,10 +1,11 @@
 import tweepy
 
 # set keys for authentication
-consumer_key = 's3ROZ7S9ptoChaQLp1MCVa5Pr'
-consumer_secret = 'oc9IfqrX3L8h9retqkbD3fsR0UcnuW5wG1KYW6K6YtXVWuAmt2'
-access_token = '1123324493412356099-F8GXg0wjDzeF1xuLYCOXx2xlaWG0wp'
-access_token_secret = 'Ao6PjZv2hml7YOllPCGWq018R36c4ifHaEGq46uGXQ12W'
+# Keys are used to add authentication, but are removed for security purposes as this is a public repo.
+consumer_key = 'XXXX' 
+consumer_secret = 'XXXX' 
+access_token = 'XXXX'
+access_token_secret = 'XXXX'
 
 # set authentication used by tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
